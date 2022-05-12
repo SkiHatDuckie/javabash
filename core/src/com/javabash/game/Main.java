bac7a4ec99f9d43361fd781ff7a9cd24a079cd15
WillyBBlue <skihatduckie@gmail.com>
2022-05-12 09:51:48 -0400

    Fix gradle classpaths

--- core/src/com/javabash/game/Main.java
+++ core/src/com/javabash/game/Main.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
